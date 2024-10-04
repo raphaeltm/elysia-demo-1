003914d2edb4c12a537fac0a62999381d43b7bb5
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2024-10-04 14:26:38 -0700

    Update index

--- elysia/src/index.ts
+++ elysia/src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 
 const app = new Elysia()
-  .get("/", () => "Elysia × Defang is awesome!")
+  .get("/", () => "Elysia × Defang is super cool!")
   .listen({
     hostname: "0.0.0.0",
     port: 3000,
